d531a18fc74c8363efd8b9e11939d0a23baa3e48
wallflower_vy <owoadevic@gmail.com>
2023-04-18 12:51:59 +0100

    minor fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      lightgreen: "#90DD3E",
-      green: "#183D36",
-      aqua: "#B0FFFC",
-      lightblue: "#DFE5FF",
-      brown: "#906D6D",
-      white: "#fff",
-      lime: "#DEEDED",
-      blue: "#003DB5",
-      background: "#F3F8FF",
+    extend: {
+      colors: {
+        lightgreen: "#90DD3E",
+        green: "#183D36",
+        aqua: "#B0FFFC",
+        lightblue: "#DFE5FF",
+        brown: "#906D6D",
+        white: "#fff",
+        lime: "#DEEDED",
+        blue: "#003DB5",
+        background: "#F3F8FF",
+      },
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+      fontFamily: {
+        plus: ["Plus Jakarta", "sans-serif"],
+      },
     },
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "976px",
-      xl: "1440px",
-    },
-    fontFamily: {
-      plus: ["Plus Jakarta", "sans-serif"],
-    },
-    extend: {},
   },
   plugins: [],
-}
+};
